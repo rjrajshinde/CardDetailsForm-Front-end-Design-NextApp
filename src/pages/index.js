fbdd1af56f45f45f64a06642b6901036d1fe5166
unknown <rajshinde333rj@gmail.com>
2023-05-05 17:09:05 +0530

    'add_background'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import CardFrontBack from "@/components/CardFrontBack";
 import Head from "next/head";
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>dfads</h1>
+        <CardFrontBack />
       </main>
     </>
   );
