6233b4c9da72f0ae4c314f845de5991b995848f4
rjrajshinde <rjrajshinde@gmail.com>
2023-05-12 21:40:01 +0530

    card pics arrangement in mobile responsive

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/index.css";
 import "@/styles/cardFrontBack.css";
+import "@/styles/cardForm.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
